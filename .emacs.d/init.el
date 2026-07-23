37b248300191ed77fbd1f9e5e3c5887a40af5ef0
hyuka <ciaoxl23s@gmail.com>
2026-07-23 11:45:33 +0900

    Load system-specific init files based on OS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,17 @@
+;;; Start of init.el  -*- lexical-binding: t; -*-
 
+(cond
+ ((eq system-type 'darwin) ;; macOS
+  (load (expand-file-name "init-macos.el" user-emacs-directory))
+  (setq custom-file (expand-file-name "init-macos.el" user-emacs-directory)))
+
+ ((eq system-type 'gnu/linux) ;; Linux
+  (load (expand-file-name "init-linux.el" user-emacs-directory))
+  (setq custom-file (expand-file-name "init-linux.el" user-emacs-directory)))
+
+ ((eq system-type 'windows-nt) ;; Windows
+  (load (expand-file-name "init-windows.el" user-emacs-directory))
+  (setq custom-file (expand-file-name "init-windows.el" user-emacs-directory)))
+ 
+ (t
+  (message "Unknown system type: %s" system-type)))
